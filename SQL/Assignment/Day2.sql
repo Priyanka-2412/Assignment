1322c1e9715ace1e198ab6c4977208ba7d325dba
Priyanka <vinupriyanka0103@gmail.com>
2025-03-18 16:43:23 +0530

    Update Day2.sql

--- SQL/Assignment/Day2.sql
+++ SQL/Assignment/Day2.sql
@@ -70,5 +70,66 @@ select * from Trainee
 delete from Skill where ID=200
 select * from Skill
 
-update Skill set Name='C#' where id =201
+update Skill set Name='C#' whereÂ idÂ =201
 select * from Skill
+
+--Sorting records
+select * from Employee order by ID desc
+
+select * from Employee order by name asc
+
+--Select record with ID 101 n 103
+select * from Employee where ID in (101,103)
+
+select * from Employee where ID between 101 and 103
+
+select * from Employee where ID=101 or ID=107
+
+select * from Employee where ID is not null
+
+select * from Employee where name like 'N%'
+
+select * from Employee where name like 'N%i'
+
+select * from Employee where name like '_r%'
+
+select * from Employee where name like '%v_'
+
+--
+select * from Employee
+select * from Employee order by name desc offset 0 row
+fetch next 2 rows only
+
+--Aggregate Function
+select count(ID) as No_Of_Employees from Employee
+
+alter table Employee add salary bigint null
+select * from Employee
+
+update Employee set salary=25000 where ID=101
+update Employee set salary=28000 where ID=102
+update Employee set salary=30000 where ID=103
+
+select * from Employee
+
+--Min & max
+select min(salary) as Minimum_salary from Employee
+select max(salary) as Maximum_salary from Employee
+
+--Avg
+select avg(salary) as Average_salary from Employee
+
+--Sum
+select sum(salary) as Total_salary from Employee
+
+--Grouping
+select * from Employee
+
+select name, license from Employee group by license,name
+
+--count of employee using same license
+select count(name) as No_of_Employees, license from Employee group by license
+
+select * from Employee
+where name like 'N%' 
+and salary >= 25000
